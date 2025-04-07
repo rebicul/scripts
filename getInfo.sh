7dc489688fa7cda099df83ec676d6ba4cc6a4a4f
Rebicul <luciano.anthony.bernal@gmail.com>
2025-04-07 14:47:09 -0400

    Add getInfo.sh script to display system information

---
+++ getInfo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+CPU=$(grep -m1 "model name" /proc/cpuinfo | cut -d':' -f2 | xargs)
+RAM=$(free -h | awk '/Mem:/ {print $2}')
+VAULT_SIZE=$(df -h | grep '/vault' | awk '{print $3 " used / " $2 " total"}')
+
+echo "\$HOSTNAME: $HOSTNAME"
+echo "CPU: $CPU"
+echo "RAM: $RAM"
+echo "/vault size: $VAULT_SIZE"
