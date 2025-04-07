aa2571e9b6cfaff85fabd283cfc20cea3d5a2dba
Rebicul <luciano.anthony.bernal@gmail.com>
2025-04-07 15:00:29 -0400

    made edits to echo statements

--- getInfo.sh
+++ getInfo.sh
@@ -5,7 +5,7 @@ CPU=$(grep -m1 "model name" /proc/cpuinfo | cut -d':' -f2 | xargs)
 RAM=$(free -h | awk '/Mem:/ {print $2}')
 VAULT_SIZE=$(df -h | grep '/vault' | awk '{print $3 " used / " $2 " total"}')
 
-echo "\$HOSTNAME: $HOSTNAME"
+echo "HOSTNAME: $HOSTNAME"
 echo "CPU: $CPU"
 echo "RAM: $RAM"
 echo "/vault size: $VAULT_SIZE"
