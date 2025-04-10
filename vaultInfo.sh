14851e00c889ed765c6a9304c0c34ad42b60b9d2
Rebicul <luciano.anthony.bernal@gmail.com>
2025-04-10 10:01:44 -0400

    including hostname on vaultInfo.sh output

--- vaultInfo.sh
+++ vaultInfo.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Capture df -h header and /vault line
+HOSTNAME=$(hostname)
 VAULT_HEADER=$(df -h | head -n 1)
 VAULT_LINE=$(df -h | grep '/vault')
 
 echo " "
-
+echo "HOSTNAME: $HOSTNAME"
 if [ -n "$VAULT_LINE" ]; then
     echo "$VAULT_HEADER"
     echo "$VAULT_LINE"
 else
     echo "/vault not found in df -h output"
 fi
-
 echo " "
