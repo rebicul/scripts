4a1efc026101269b4d7b051cafb871e52cbc26b0
Rebicul <luciano.anthony.bernal@gmail.com>
2025-05-12 09:15:17 -0400

    updating script to only show vault info

--- vaultInfo.sh
+++ vaultInfo.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-HOSTNAME=$(hostname)
+# Get df -h header
 VAULT_HEADER=$(df -h | head -n 1)
-VAULT_LINE=$(df -h | grep '/vault')
 
-echo " "
-echo "HOSTNAME: $HOSTNAME"
+# Get the /vault line (if it exists)
+VAULT_LINE=$(df -h | awk '$NF == "/vault"')
+
+# Output
+echo ""
 if [ -n "$VAULT_LINE" ]; then
     echo "$VAULT_HEADER"
     echo "$VAULT_LINE"
 else
     echo "/vault not found in df -h output"
 fi
-echo " "
+echo ""
